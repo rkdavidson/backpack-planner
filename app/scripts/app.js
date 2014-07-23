1f5ac596c003a07d2b8db0989e334b4f21152f64
Ryan Davidson <ryanrkd2112@gmail.com>
2014-07-22 23:29:50 -0400

    Initial commit, food data loaded from JSON file and displayed in ng-grid. Basic dependencies integrated, nvd3 ng-grid etc

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jmtFoodPlannerApp
+ * @description
+ * # jmtFoodPlannerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jmtFoodPlannerApp', [
+    'ngGrid',
+    'nvd3ChartDirectives'
+    ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jmtFoodPlannerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jmtFoodPlannerApp
+ */
+angular.module('jmtFoodPlannerApp')
+  .controller('MainCtrl', ['$scope', '$log', 'Items', function ($scope, $log, Items) {
+    $scope.$log = $log;
+    $scope.gridOptions = { data: 'foodData' };
+
+    Items.then( function(parsedCsvData) {
+        $scope.foodData = parsedCsvData;
+    });
+
+  }])
+  .factory('Items', ['$http', '$q', function($http, $q) {
+    var Url = 'data/food-v1.json';
+
+    var ItemsDefer = $q.defer();
+    $http.get(Url).then( function(response) {
+       ItemsDefer.resolve( response.data );
+    });
+
+    return ItemsDefer.promise;
+  }]);
